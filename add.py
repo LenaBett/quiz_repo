1146b9f81928ca1ad1aa07f25ea7383976b2f85f
LenaBett <rheabett@gmail.com>
2023-10-26 15:25:51 +0300

    CI/CD2

---
+++ add.py
@@ -0,0 +1,7 @@
+
+
+def add(num1,num2):
+ return num1 + num2
+
+# def test_sum():
+#  assert add(2,3) == 5
---
+++ test_add.py
@@ -0,0 +1,5 @@
+from add import add
+
+def test_addition():
+    result=add(4,8)
+    assert result == 12
